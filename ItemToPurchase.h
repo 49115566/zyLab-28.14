58b881ca0f61521c12225538c04bbcf32c5441f9
49115566 <153975225+49115566@users.noreply.github.com>
2024-04-22 11:32:05 -0500

    Update ItemToPurchase.h
    Added the rest of the code to ItemToPurchase

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,26 +1,28 @@
 #ifndef ITEM_TO_PURCHASE_H
 #define ITEM_TO_PURCHASE_H
-
 #include <string>
 using namespace std;
 
-
 class ItemToPurchase {
 public:
-	ItemToPurchase();
+	ItemToPurchase(string n = "", string d = "", double p = 0, int q = 0);
+
 	void SetName(string n);
+	void SetDescription(string d);
 	void SetPrice(double p);
 	void SetQuantity(int q);
 	string GetName();
+	string GetDescription();
 	double GetPrice();
 	int GetQuantity();
+
+	void PrintItemCost();
+	void PrintItemDescription();
 private:
 	string name;
+	string itemDescription
 	double price;
 	int quantity;
 };
-
-
-/* code to be added here */
 
 #endif
