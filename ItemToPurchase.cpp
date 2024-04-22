c7e7a47288698791aab290949e241f3c5e7eba2a
49115566 <153975225+49115566@users.noreply.github.com>
2024-04-22 11:26:44 -0500

    Update some of ItemToPurchase.cpp
    I am also unsure if this is complete yet; I need to review the assignment.

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,34 @@
-//Code goes here
+#include <iostream>
+using namespace std;
+
+#include "ItemToPurchase.h"
+
+ItemToPurchase::ItemToPurchase() {
+	name = "none";
+	price = 0;
+	quantity = 0;
+}
+
+void ItemToPurchase::SetName(string n) {
+	name = n;
+}
+
+void ItemToPurchase::SetPrice(double p) {
+	price = p;
+}
+
+void ItemToPurchase::SetQuantity(int q) {
+	quantity = q;
+}
+
+string ItemToPurchase::GetName() {
+	return name;
+}
+
+double ItemToPurchase::GetPrice() {
+	return price;
+}
+
+int ItemToPurchase::GetQuantity() {
+	return quantity;
+}
