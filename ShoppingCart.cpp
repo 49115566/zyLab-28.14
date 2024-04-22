27bb045d043f19ce6a641ca9b75f4f5a47f554e3
Alex Gao <143097174+Ga0-Alex@users.noreply.github.com>
2024-04-22 11:30:59 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,15 @@
-//Code goes here.
+#include "ItemToPurchase.h"
+#include <vector>
+#include <string>
+
+class ShoppingCart {
+private: 
+string customerName;
+string currentDate;
+vector < ItemToPurchase > cartItems;
+
+public:
+ShoppingCart();
+ShoppingCart(string name,
+
+};
