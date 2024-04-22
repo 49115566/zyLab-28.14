f26935cd81a9814ee96b2eadbe4afec02b21b654
Alex Gao <143097174+Ga0-Alex@users.noreply.github.com>
2024-04-22 11:49:46 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,11 @@
-//Code goes here.
+#include "ShoppingCart.h"
+#include <iostream>
+using namespace std;
+
+ShoppingCart::ShoppingCart() : customerName("none"), currentData("January 1, 2016") {}
+
+ShoppingCart::ShoppingCart(string name, string data) : customerName(name), currentDate(date) {}
+
+string ShoppingCart::GetCustomerName() const{
+  return customerName;
+}
