4aa2a11f551e3e40b6eea09447e34f8c0efc5322
49115566 <153975225+49115566@users.noreply.github.com>
2024-04-22 11:40:04 -0500

    Update ItemToPurchase.cpp
    Finished updates to ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
+#include "ItemToPurchase.h"
 using namespace std;
 
-#include "ItemToPurchase.h"
-
-ItemToPurchase::ItemToPurchase() {
-	name = "none";
-	price = 0;
-	quantity = 0;
+ItemToPurchase::ItemToPurchase(string n, string d, double p, int q) {
+	name = n;
+	itemDescription = d;
+	price = p;
+	quantity = q;
 }
 
 void ItemToPurchase::SetName(string n) {
 	name = n;
 }
-
+void ItemToPurchase::SetDescription(string n) {
+	itemDescription = d;
+}
 void ItemToPurchase::SetPrice(double p) {
 	price = p;
 }
-
 void ItemToPurchase::SetQuantity(int q) {
 	quantity = q;
 }
-
 string ItemToPurchase::GetName() {
 	return name;
 }
-
+string ItemToPurchase::GetDescription() {
+	return itemDescription;
+}
 double ItemToPurchase::GetPrice() {
 	return price;
 }
-
 int ItemToPurchase::GetQuantity() {
 	return quantity;
+}
+
+void ItemToPurchase::PrintItemCost() {
+	cout << name << ' ' << quantity << " @ $" << price << " = $" << quantity*price << endl;
+}
+void ItemToPurchase::PrintItemDescription() {
+	cout << name << ": " << itemDescription << endl;
 }
