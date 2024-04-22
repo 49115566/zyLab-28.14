83a0e4056a0bb186f746a544c15cdc25a4ac8991
49115566 <153975225+49115566@users.noreply.github.com>
2024-04-22 11:25:10 -0500

    Added some code to ItemToPurchase.h
    I don't know if this is complete yet; I need to review instructions

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,1 +1,26 @@
-//Code goes here
+#ifndef ITEM_TO_PURCHASE_H
+#define ITEM_TO_PURCHASE_H
+
+#include <string>
+using namespace std;
+
+
+class ItemToPurchase {
+public:
+	ItemToPurchase();
+	void SetName(string n);
+	void SetPrice(double p);
+	void SetQuantity(int q);
+	string GetName();
+	double GetPrice();
+	int GetQuantity();
+private:
+	string name;
+	double price;
+	int quantity;
+};
+
+
+/* code to be added here */
+
+#endif
